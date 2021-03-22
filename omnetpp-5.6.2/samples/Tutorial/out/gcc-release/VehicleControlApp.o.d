ba8d4a29b52b26091bbfcb704f9ff21a745b0d3f
Aadreesh <aadreeshsahu@gmail.com>
2021-03-22 11:06:53 +0530

    Creating speed control application

---
+++ omnetpp-5.6.2/samples/Tutorial/out/gcc-release/VehicleControlApp.o.d
@@ -0,0 +1,154 @@
+out/gcc-release//VehicleControlApp.o: VehicleControlApp.cc \
+ VehicleControlApp.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/veins.h \
+ /home/aadreesh/omnetpp-5.6.2/include/omnetpp.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/application/ieee80211p/DemoBaseApplLayer.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/modules/BaseApplLayer.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/modules/BaseLayer.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/modules/BatteryAccess.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/modules/BaseModule.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/HostState.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/modules/BaseBattery.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/PassedMessage.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/SimpleAddress.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/utility/Consts80211p.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/utility/ConstsPhy.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/messages/BaseFrame1609_4_m.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/messages/DemoServiceAdvertisement_m.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/Coord.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/FWMath.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/messages/DemoSafetyMessage_m.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/connectionManager/ChannelAccess.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/AntennaPosition.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/FindModule.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/modules/BaseMobility.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/Move.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/modules/BaseWorldUtility.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/Heading.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mac/ieee80211p/DemoBaseApplLayerToMac1609_4Interface.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/NetwToMacControlInfo.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCIMobility.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCIScenarioManager.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/connectionManager/BaseConnectionManager.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/connectionManager/NicEntry.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/utility/HasLogProxy.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/obstacle/ObstacleControl.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/obstacle/Obstacle.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/world/annotations/AnnotationManager.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/utility/BBoxLookup.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/obstacle/VehicleObstacleControl.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/obstacle/MobileHostObstacle.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCIBuffer.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCIConstants.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCIColor.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCIConnection.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCICoord.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/VehicleSignal.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/EnumBitset.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCICommandInterface.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h \
+ /home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/application/traci/MyVeinsApp.h
+
+VehicleControlApp.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/veins.h:
+
+/home/aadreesh/omnetpp-5.6.2/include/omnetpp.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/application/ieee80211p/DemoBaseApplLayer.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/modules/BaseApplLayer.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/modules/BaseLayer.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/modules/BatteryAccess.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/modules/BaseModule.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/HostState.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/modules/BaseBattery.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/PassedMessage.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/SimpleAddress.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/utility/Consts80211p.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/utility/ConstsPhy.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/messages/BaseFrame1609_4_m.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/messages/DemoServiceAdvertisement_m.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/Coord.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/FWMath.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/messages/DemoSafetyMessage_m.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/connectionManager/ChannelAccess.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/AntennaPosition.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/FindModule.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/modules/BaseMobility.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/Move.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/modules/BaseWorldUtility.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/Heading.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mac/ieee80211p/DemoBaseApplLayerToMac1609_4Interface.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/NetwToMacControlInfo.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCIMobility.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCIScenarioManager.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/connectionManager/BaseConnectionManager.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/connectionManager/NicEntry.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/utility/HasLogProxy.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/obstacle/ObstacleControl.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/obstacle/Obstacle.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/world/annotations/AnnotationManager.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/utility/BBoxLookup.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/obstacle/VehicleObstacleControl.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/obstacle/MobileHostObstacle.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCIBuffer.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCIConstants.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCIColor.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCIConnection.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCICoord.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/VehicleSignal.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/base/utils/EnumBitset.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/mobility/traci/TraCICommandInterface.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h:
+
+/home/aadreesh/omnetpp-5.6.2/veins-5.1/veins-veins-5.1/src/veins/modules/application/traci/MyVeinsApp.h:
